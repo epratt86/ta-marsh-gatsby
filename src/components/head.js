810d862b606376bfef1b63a978affa95571e8aed
Eric Pratt <ericpratt86@gmail.com>
2019-05-21 11:05:20 -0700

    meta-data

--- src/components/head.js
+++ src/components/head.js
@@ -15,6 +15,17 @@ const Head = ({ title }) => {
 
   return (
     <Helmet title={`${title} | ${data.site.siteMetadata.title}`}>
+      <html lang="en" />
+      <meta charSet="utf-8" />
+      <meta name="docsearch:version" content="2.0" />
+      <meta
+        name="viewport"
+        content="width=device-width,initial-scale=1,shrink-to-fit=no,viewport-fit=cover"
+      />
+      <meta
+        name="description"
+        content="TA Marsh Roofing provides multiple services across San Diego County. Find out how we can help you!"
+      />
       <link
         href="https://fonts.googleapis.com/css?family=Raleway"
         rel="stylesheet"
